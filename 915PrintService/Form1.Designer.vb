f91025b434d85385341471277c4de04b83ec4f05
9646516 <zyq855@gmail.com>
2019-10-22 18:21:13 +0800

    add print

--- 915PrintService/Form1.Designer.vb
+++ 915PrintService/Form1.Designer.vb
@@ -22,10 +22,72 @@ Partial Class Form1
     '不要使用代码编辑器修改它。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.btnOpen = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.btnPrint = New System.Windows.Forms.Button()
+        Me.PrintDialog1 = New System.Windows.Forms.PrintDialog()
+        Me.PrintDocument1 = New System.Drawing.Printing.PrintDocument()
+        Me.SuspendLayout()
+        '
+        'btnOpen
+        '
+        Me.btnOpen.Location = New System.Drawing.Point(17, 263)
+        Me.btnOpen.Name = "btnOpen"
+        Me.btnOpen.Size = New System.Drawing.Size(127, 52)
+        Me.btnOpen.TabIndex = 2
+        Me.btnOpen.Text = "Open"
+        Me.btnOpen.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(43, 22)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(279, 222)
+        Me.RichTextBox1.TabIndex = 4
+        Me.RichTextBox1.Text = ""
+        '
+        'btnPrint
+        '
+        Me.btnPrint.Location = New System.Drawing.Point(195, 263)
+        Me.btnPrint.Name = "btnPrint"
+        Me.btnPrint.Size = New System.Drawing.Size(127, 52)
+        Me.btnPrint.TabIndex = 5
+        Me.btnPrint.Text = "GAO"
+        Me.btnPrint.UseVisualStyleBackColor = True
+        '
+        'PrintDialog1
+        '
+        Me.PrintDialog1.UseEXDialog = True
+        '
+        'PrintDocument1
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(374, 340)
+        Me.Controls.Add(Me.btnPrint)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.btnOpen)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents btnOpen As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents btnPrint As Button
+    Friend WithEvents PrintDialog1 As PrintDialog
+    Friend WithEvents PrintDocument1 As Printing.PrintDocument
 End Class
--- 915PrintService/Form1.vb
+++ 915PrintService/Form1.vb
@@ -1,3 +1,62 @@
-﻿Public Class Form1
+﻿Imports System.IO
+Imports System.Drawing.Printing
 
+Public Class Form1
+    Private PrintPageSettings As New PageSettings
+    Private StringToPrint As String
+    Private TitleFont As New Font("Arial", 12, FontStyle.Bold)
+    Private PrintFont As New Font("Arial", 10)
+    Private SubmitTime = New Date().ToString()
+    Private SubmitTeam = "YTS1999"
+    Private SubmitProblem = "YTS1999"
+    Private Sub btnOpen_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnOpen.Click
+        Dim FilePath As String
+        OpenFileDialog1.Filter = "Text files (*.txt)|*.txt"
+        OpenFileDialog1.ShowDialog()
+        If OpenFileDialog1.FileName <> "" Then
+            FilePath = OpenFileDialog1.FileName
+            Try
+                Dim MyFileStream As New FileStream(FilePath, FileMode.Open)
+                RichTextBox1.LoadFile(MyFileStream, RichTextBoxStreamType.PlainText)
+                MyFileStream.Close()
+                btnPrint.Enabled = True
+            Catch ex As Exception
+                MessageBox.Show(ex.Message)
+            End Try
+        End If
+    End Sub
+
+    Private Sub btnPrint_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnPrint.Click
+        Try
+            PrintDocument1.DefaultPageSettings = PrintPageSettings
+            StringToPrint = RichTextBox1.Text
+            PrintDialog1.Document = PrintDocument1
+            PrintDocument1.Print()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub PrintDocument1_PrintPage(ByVal sender As System.Object, ByVal e As System.Drawing.Printing.PrintPageEventArgs) Handles PrintDocument1.PrintPage
+        Dim numChars As Integer
+        Dim numLines As Integer
+        Dim stringForPage As String
+        Dim strFormat As New StringFormat
+        Dim titleRect As New RectangleF(e.MarginBounds.Left, e.MarginBounds.Top - 30, e.MarginBounds.Width, e.MarginBounds.Height)
+        Dim lineRect As New RectangleF(e.MarginBounds.Left, e.MarginBounds.Top - 20, e.MarginBounds.Width, e.MarginBounds.Height)
+        Dim rectDraw As New RectangleF(e.MarginBounds.Left, e.MarginBounds.Top, e.MarginBounds.Width, e.MarginBounds.Height)
+        Dim sizeMeasure As New SizeF(e.MarginBounds.Width, e.MarginBounds.Height - PrintFont.GetHeight(e.Graphics))
+        strFormat.Trimming = StringTrimming.Word
+        e.Graphics.MeasureString(StringToPrint, PrintFont, sizeMeasure, strFormat, numChars, numLines)
+        stringForPage = StringToPrint.Substring(0, numChars)
+        e.Graphics.DrawString(SubmitTime + "      " + SubmitProblem + "     " + SubmitTeam + vbCrLf, TitleFont, Brushes.Black, titleRect, strFormat)
+        e.Graphics.DrawString("--------------------------------------------------------------------------------------------------------" + vbCrLf, TitleFont, Brushes.Black, lineRect, strFormat)
+        e.Graphics.DrawString(stringForPage, PrintFont, Brushes.Black, rectDraw, strFormat)
+        If numChars < StringToPrint.Length Then
+            StringToPrint = StringToPrint.Substring(numChars)
+            e.HasMorePages = True
+        Else
+            e.HasMorePages = False
+        End If
+    End Sub
 End Class
